0c98d6be3ddb4a9b90fb01973b7ec15bc8532111
killer <suyu9549@gmail.com>
2024-10-04 15:04:52 +0900

    v0.5

--- wekk06/main.go
+++ wekk06/main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func main() {
-	var i int = 13
-	f := 12.8
+	i := 13   //var i int = 13
+	f := 12.8 //var f float64=12.8
+	c1 := `2`
+	c2 := `김`
 	//fmt.Print("value i: %d,value f:%f", i, f)
 	fmt.Printf("value i: %d,value f:%f\n", i, f)
 
 	//fmt.Print("%d * %f = %f\n", i, f, i*f)
 	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
 	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	fmt.Println(reflect.TypeOf(c1), reflect.TypeOf(c2))
+	fmt.Println(c1, c2)
 }
