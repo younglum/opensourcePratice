01a6e38c8420fe4e24a8904ce99aea159fe92993
killer <suyu9549@gmail.com>
2024-10-04 15:42:37 +0900

    v0.6

--- wekk06/main.go
+++ wekk06/main.go
@@ -9,7 +9,13 @@ func main() {
 	i := 13   //var i int = 13
 	f := 12.8 //var f float64=12.8
 	c1 := `2`
-	c2 := `김`
+	c2 := `김` //44608,유니코드
+	var t float64
+	var a int
+	var o bool
+	mySchoolAccount := 5.0 //Camelcase 표기법
+	fmt.Println(t, a, o, mySchoolAccount)
+	fmt.Printf("%f %d %t\n", t, a, o)
 	//fmt.Print("value i: %d,value f:%f", i, f)
 	fmt.Printf("value i: %d,value f:%f\n", i, f)
 
@@ -18,4 +24,9 @@ func main() {
 	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 	fmt.Println(reflect.TypeOf(c1), reflect.TypeOf(c2))
 	fmt.Println(c1, c2)
+
+	mySchoolAccount = 2.7
+	c = 3
+	fmt.Print("\n\n", mySchoolAccount > float64(c))
+
 }
