fbe9bfe48d4103ce1bddd554a855eb5e18f4e387
killer <suyu9549@gmail.com>
2024-10-11 14:26:52 +0900

    v1.0

--- week07/main.go
+++ week07/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
-	// 오늘은 2024년 10월 11일 입니다.//
-	var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
+	in := bufio.NewReader(os.Stdin)
+	fmt.Print("이름을 입력하세요.")
+	name, err := in.ReadString('\n')
+	fmt.Println(name)
+	fmt.Println(err)
+	/*var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
 	armyFixed := strings.NewReplacer("$", "국")
 	fmt.Println(army)
-	fmt.Println(armyFixed.Replace(army))
+	fmt.Println(armyFixed.Replace(army))*/
 	/*var now time.Time = time.Now()
 	var mouth int = int(now.Month())
 	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.", now.Year(), mouth, int(now.Day()))
