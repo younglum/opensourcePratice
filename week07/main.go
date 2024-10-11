e185973a4eb37f59d4c858e04cab279e356195aa
killer <suyu9549@gmail.com>
2024-10-11 14:12:54 +0900

    v0.9

--- week07/main.go
+++ week07/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
 	// 오늘은 2024년 10월 11일 입니다.//
-	var now time.Time = time.Now()
+	var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
+	armyFixed := strings.NewReplacer("$", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
+	/*var now time.Time = time.Now()
 	var mouth int = int(now.Month())
 	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.", now.Year(), mouth, int(now.Day()))
-	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.", now.Hour(), now.Minute(), now.Second())
+	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.", now.Hour(), now.Minute(), now.Second())*/
 }
