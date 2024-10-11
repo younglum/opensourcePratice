f983583b08ae95acb645b329d07f9c03e0c92dbc
killer <suyu9549@gmail.com>
2024-10-11 14:00:53 +0900

    v0.8

--- week07/main.go
+++ week07/main.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
+	// 오늘은 2024년 10월 11일 입니다.//
 	var now time.Time = time.Now()
 	var mouth int = int(now.Month())
-	fmt.Printf("%d", mouth)
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.", now.Year(), mouth, int(now.Day()))
+	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.", now.Hour(), now.Minute(), now.Second())
 }
