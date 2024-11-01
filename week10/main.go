e21273af478b4ef5533b0ee8f3b1070698849098
killer <suyu9549@gmail.com>
2024-11-01 14:18:42 +0900

    v1.7

--- week10/main.go
+++ week10/main.go
@@ -21,18 +21,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	counts := 0
+	//counts := 0
+	var isPrime bool = true //flag변수 , 사용시 가독성과 메모리 용량의 효율성을 챙길 수 있음
 	j := 2
 	for j < n {
 		if n%j == 0 {
-			counts++
+			//counts++
+			isPrime = false
 		}
 
 		j++
 	}
-	if counts == 0 {
+	if isPrime {
+		//if counts == 0 {
 		fmt.Printf("%d is prime number.", n)
 	} else {
-		fmt.Printf("%d is not prime number\n count : %d", n, counts)
+		fmt.Printf("%d is not prime number\n count :", n)
+		//fmt.Printf("%d is not prime number\n count : %d", n, counts)
 	}
 }
