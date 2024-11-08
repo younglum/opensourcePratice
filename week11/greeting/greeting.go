deb93c42076556db8136c5809530493d9b589881
killer <suyu9549@gmail.com>
2024-11-08 14:56:12 +0900

    v2.4

---
+++ week11/greeting/greeting.go
@@ -0,0 +1,18 @@
+package greeting
+
+import (
+	"fmt"
+)
+
+func hi(name string) {
+	fmt.Printf("Hi %s\n", name)
+}
+
+func hello(name string) {
+	fmt.Printf("Hello %s\n", name)
+}
+
+func EnglishGreetings(name string) {
+	hello(name)
+	hi(name)
+}
--- week11/main.go
+++ week11/main.go
@@ -1,62 +1,65 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
+import "test/greeting" //go.mod파일명이 test이므로 test/greeting을 적어야 함.
 
-var counts int = 0
+//var counts int = 0
 
-func isPrime(n int) bool {
-	if n < 0 {
-		return false
-	} else if n == 1 || n == 0 {
-		fmt.Printf("1또는 0이 입력되었습니다.\n")
-		return false
-	} else if n == 2 {
-		return true
-	} else if n%2 == 0 {
-		return false
-	} else {
-		j := 3
-		for j*j <= n {
-			if n%j == 0 {
-				return false
+/*
+	func isPrime(n int) bool {
+		if n < 0 {
+			return false
+		} else if n == 1 || n == 0 {
+			fmt.Printf("1또는 0이 입력되었습니다.\n")
+			return false
+		} else if n == 2 {
+			return true
+		} else if n%2 == 0 {
+			return false
+		} else {
+			j := 3
+			for j*j <= n {
+				if n%j == 0 {
+					return false
+				}
+				j += 2
+				counts++
 			}
-			j += 2
-			counts++
 		}
+		return true
 	}
-	return true
-}
 
-func GetIntager() int {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Print("input your start number: ")
-	a, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	func GetIntager() (int, error) {
+		in := bufio.NewReader(os.Stdin)
+		fmt.Print("input your start number: ")
+		a, err := in.ReadString('\n')
+		if err != nil {
+			return 0, err
+		}
+		a = strings.TrimSpace(a)
+		n1, err := strconv.Atoi(a)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return n1, nil
 	}
-	a = strings.TrimSpace(a)
-	n1, err := strconv.Atoi(a)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return n1
-}
-
+*/
 func main() {
-	n1 := GetIntager()
-	n2 := GetIntager()
+	/*n1, err := GetIntager()
+	if err != nil {
+		log.Fatal(err)
+	}
+	n2, err := GetIntager()
+	if err != nil {
+		log.Fatal(err)
+	}
 	for j := n1; j <= n2; j++ {
 		if isPrime(j) {
 			fmt.Printf("%d is prime number.\n count : %d\n", j, counts)
 		} else {
 			fmt.Printf("%d is not prime number\n count : %d\n", j, counts)
 		}
-	}
+	}*/
+	//greeting.Hello("김인하")
+	//greeting.Hi("이인하")
+	greeting.EnglishGreetings("Inha")
 }
