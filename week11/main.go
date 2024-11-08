d32a3c2c5a1a97f320e55e3cba2edd6acd0326ad
killer <suyu9549@gmail.com>
2024-11-08 14:01:42 +0900

    v2.3

--- week11/main.go
+++ week11/main.go
@@ -12,27 +12,21 @@ import (
 var counts int = 0
 
 func isPrime(n int) bool {
-	//var isPrime bool = true flag변수 , 사용시 가독성과 메모리 용량의 효율성을 챙길 수 있음
 	if n < 0 {
 		return false
-	} else if n == 1 || n == 0 { //입력받은 값이 1보다 작거나 같을때 == 1 또는 음수일 때
-		//isPrime = false
+	} else if n == 1 || n == 0 {
 		fmt.Printf("1또는 0이 입력되었습니다.\n")
 		return false
 	} else if n == 2 {
-		//isPrime = true
 		return true
-	} else if n%2 == 0 { //2를 제외한 짝수는 모두 소수가 아님,그래서 짝수일 때 무조건 소수가 아님을 출력
-		//isPrime = false
+	} else if n%2 == 0 {
 		return false
 	} else {
 		j := 3
-		//for j <= int(math.Sqrt(float64(n)))
 		for j*j <= n {
 			if n%j == 0 {
 				return false
 			}
-			//fmt.Printf("%d ", j) //for문의 반복횟수 출력
 			j += 2
 			counts++
 		}
@@ -41,33 +35,33 @@ func isPrime(n int) bool {
 }
 
 func main() {
-	//fmt.Printf("%f\n", math.Sqrt(16.8)) //16.8에 루트를 씌운 값을 출력
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("input your number: ")
-	i, err := in.ReadString('\n') //입력을 문자열로 받음
+	fmt.Print("input your start number: ")
+	a, err := in.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	i = strings.TrimSpace(i)
-	n, err := strconv.Atoi(i)
+	a = strings.TrimSpace(a)
+	n1, err := strconv.Atoi(a)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//counts := 0
-	/*j := 2
-	for j < n {
-		if n%j == 0 {
-			//counts++
-			isPrime = false
+	bn := bufio.NewReader(os.Stdin)
+	fmt.Print("input your end number: ")
+	b, err := bn.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	b = strings.TrimSpace(b)
+	n2, err := strconv.Atoi(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for j := n1; j <= n2; j++ {
+		if isPrime(j) {
+			fmt.Printf("%d is prime number.\n count : %d\n", j, counts)
+		} else {
+			fmt.Printf("%d is not prime number\n count : %d\n", j, counts)
 		}
-
-		j++
-	}*/
-	if isPrime(n) {
-		//if counts == 0 {
-		fmt.Printf("%d is prime number.\n count : %d", n, counts)
-	} else {
-		fmt.Printf("%d is not prime number\n count : %d", n, counts)
-		//fmt.Printf("%d is not prime number\n count : %d", n, counts)
 	}
 }
