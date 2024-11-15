46471a4b03ddad955fc3bd5e9ba15759b384a07e
killer <suyu9549@gmail.com>
2024-11-15 14:20:24 +0900

    v2.7

--- week12/main.go
+++ week12/main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	var scores [3]int
 	scores[1] = 90
-	println(scores[1])
+	fmt.Println(scores)
+	fmt.Printf("%#v\n", scores) //#v하면 배열의 리터럴을 그대로 보여준다
+
+	// 	var dates [3]time.Time
+	// 	dates[1] = time.Unix(1447920000, 0)
+	// 	fmt.Print(dates[1])
+
+	dates := [3]time.Time{
+		time.Unix(0, 0),
+		time.Unix(1, 0),
+		time.Unix(14479200000, 0)}
+
+	fmt.Println(dates[0], dates[1], dates[2])
+	fmt.Printf("%#v\n", dates)
+
 }
