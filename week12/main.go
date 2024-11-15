a1aee7320f7c692d0e0ad409db47da9e05f57e98
killer <suyu9549@gmail.com>
2024-11-15 14:47:32 +0900

    v2.7

--- week12/main.go
+++ week12/main.go
@@ -8,8 +8,12 @@ import (
 func main() {
 	var scores [3]int
 	scores[1] = 90
-	fmt.Println(scores)
-	fmt.Printf("%#v\n", scores) //#v하면 배열의 리터럴을 그대로 보여준다
+	//fmt.Println(scores)
+	//fmt.Printf("%#v\n", scores) //#v하면 배열의 리터럴을 그대로 보여준다
+
+	for i := 0; i < len(scores); i++ {
+		fmt.Printf("%d\n", scores[i])
+	}
 
 	// 	var dates [3]time.Time
 	// 	dates[1] = time.Unix(1447920000, 0)
@@ -20,7 +24,11 @@ func main() {
 		time.Unix(1, 0),
 		time.Unix(14479200000, 0)}
 
-	fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Printf("%#v\n", dates)
+	fmt.Print("")
+	// fmt.Println(dates[0], dates[1], dates[2])
+	// fmt.Printf("%#v\n", dates)
+	for i, date := range dates {
+		fmt.Println(i, date)
+	}
 
 }
