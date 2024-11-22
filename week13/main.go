d2c35a9efd59a136cc5d490c5ac5994d72a1c8d6
killer <suyu9549@gmail.com>
2024-11-22 14:11:26 +0900

    v3.1

--- week13/main.go
+++ week13/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
-	fmt.Print("hellow")
+	var arrays [5]float64 = [5]float64{3.5, 4.1, 4.5, 5.0, 4.2}
+	arrays_slice := arrays[1:4] //슬라이스 생성
+	//arrays_slice[1] = 2.71
+	arrays[2] = 2.71 //어디서 바꾸든 둘 다 출력이 동일해짐=슬라이싱은 array를 포인터로 가르키는것과 동일
+	arrays_slice = append(arrays_slice, 4.6)
+	fmt.Println(arrays, reflect.TypeOf(arrays))
+	fmt.Println(arrays_slice, reflect.TypeOf(arrays_slice))
 }
