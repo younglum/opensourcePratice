c1257ff2c1053a1fb74e7afc2cd84937e12863d3
killer <suyu9549@gmail.com>
2024-11-22 15:12:01 +0900

    v3.2

--- week13/main.go
+++ week13/main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
-	var arrays [5]float64 = [5]float64{3.5, 4.1, 4.5, 5.0, 4.2}
-	arrays_slice := arrays[1:4] //슬라이스 생성
-	//arrays_slice[1] = 2.71
-	arrays[2] = 2.71 //어디서 바꾸든 둘 다 출력이 동일해짐=슬라이싱은 array를 포인터로 가르키는것과 동일
-	arrays_slice = append(arrays_slice, 4.6)
-	fmt.Println(arrays, reflect.TypeOf(arrays))
-	fmt.Println(arrays_slice, reflect.TypeOf(arrays_slice))
+	//fmt.Println(os.Args[1:], len(os.Args))
+	Slice := os.Args[1:] //실행하는 파일이름을 출력하지 않게 자름
+	fmt.Println(Slice[1])
+	for _, slice := range Slice {
+		fmt.Println(slice)
+	}
+	Slice = append(Slice, "Forever", "happy")
+	fmt.Printf("%s  %d", Slice, len(Slice))
+
+	/*
+		var emptySliace []bool
+		//emptySliace = make([]bool, 0)
+		fmt.Printf("%#v %d\n", emptySliace, len(emptySliace))
+
+		if len(emptySliace) == 0 { //emptySlice의 크기가 0이면 실행. 선언하고 아무것도 안 넣었기 때문에 0
+			emptySliace = append(emptySliace, true)
+		}
+		fmt.Printf("%#v  %d\n", emptySliace, len(emptySliace))
+
+
+			var arrays [5]float64 = [5]float64{3.5, 4.1, 4.5, 5.0, 4.2}
+			arrays_slice := arrays[1:4] //슬라이스 생성
+			//arrays_slice[1] = 2.71
+			arrays[2] = 2.71 //어디서 바꾸든 둘 다 출력이 동일해짐=슬라이싱은 array를 포인터로 가르키는것과 동일
+			arrays_slice = append(arrays_slice, 4.6)
+			fmt.Println(arrays, reflect.TypeOf(arrays))
+			fmt.Println(arrays_slice, reflect.TypeOf(arrays_slice))
+	*/
 }
